8b50c98d0c416fbdb4bd08806589f874fed8989c
Xavier Gonzalez <zechomaru@gmail.com>
2015-03-11 12:17:56 -0430

    validates de modelos

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,4 +1,8 @@
 class Mentor < ActiveRecord::Base
 	has_attached_file :avatar, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  	validates :name, :lastname, :age, :description, :email, :business, presence: true
+  	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  	validates :age, numericality: true
 end
