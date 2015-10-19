f0b801d97bd0ad1f4dc95992c3443f2b99bc9ebe
user <email@academiahack.com.ve@4geeks.co>
2015-10-19 05:36:02 +0000

    shared directories configured for paperclip to work

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :repo_url, 'git@github.com:HackProyectos/aje_web.git'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -55,7 +55,7 @@ namespace :deploy do
     end
   end
 
-  after :compile_assets, :build_missing_paperclip_styles
+  after :deploy, :build_missing_paperclip_styles
   after :publishing, :restart
   after :finishing, 'deploy:cleanup'
 
