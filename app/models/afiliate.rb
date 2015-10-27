7b4ac9a7b784245bc6e47163c29245e067d3509a
user <email@academiahack.com.ve@4geeks.co>
2015-10-27 06:45:03 +0000

    cambiosfinalesen las secciones y responsive de las secciones, cambio del email de contacto

--- app/models/afiliate.rb
+++ app/models/afiliate.rb
@@ -1,7 +1,7 @@
 class Afiliate < ActiveRecord::Base
-	  validates :name, :lastname, :business, :ci, :phone, :email, presence: true
-	  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
-	  validates :ci, numericality: true
-	  validates :phone, numericality: true
+	  validates :name, :lastname, :business, :ci, :phone, :email, :presence => {:message => "No puede estar vacío"}
+	  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "No es un correo electrónico válido" }, :uniqueness => {:message => "Ya existe alguien afiliado con ese correo"}
+	  validates :ci, :numericality => {:message => "Debe ser un número"}
+	  validates :phone, :numericality => {:message => "Debe ser un número"}
 
 end
